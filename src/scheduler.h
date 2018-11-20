59f6161ad2718b20d5d1b91a1a788a01012f87b4
Jonathan Davis <jonathan.davis@uconn.edu>
2018-11-20 16:20:28 -0500

    Add documentation comments to scheduler.h

--- src/scheduler.h
+++ src/scheduler.h
@@ -3,8 +3,22 @@
 
 #include "utils/matrix.h"
 
+/* Takes a thread cost matrix c and sorts it using the FCFS scheduling algorithm.
+ * c - (3, n) Matrix representing PLJ, LASU, and RJ costs. n is the number of threads.
+ * Returns c scheduled by FCFS.
+ */
 TMatrix* fcfs(TMatrix *c);
+
+/* Takes a thread cost matrix c and sorts it using the LRJF scheduling algorithm.
+ * c - (3, n) Matrix representing PLJ, LASU, and RJ costs. n is the number of threads.
+ * Returns c scheduled by LRJF.
+ */
 TMatrix* lrjf(TMatrix *c);
+
+/* Takes a thread cost matrix c and sorts it using the heuristic scheduling algorithm.
+ * c - (3, n) Matrix representing PLJ, LASU, and RJ costs. n is the number of threads.
+ * Returns c scheduled by heuristic comparison algorithm.
+ */
 TMatrix* heuristic(TMatrix *c);
 
 #endif
