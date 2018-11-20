db137cddbeac34a311e8894c6df084812fa6d91e
Jonathan Davis <jonathan.davis@uconn.edu>
2018-11-20 18:16:41 -0500

    Add mergesort test to main function

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,6 +1,41 @@
-/* scheduler.c
- * Main file for running the scheduling program for LRJF, FCFS or heuristically.
- */
+#include <stdlib.h>
+#include <stdio.h>
+#include "scheduler.h"
 
-#import 
 
+int sort(TElement a, TElement b)
+{
+    return b < a;
+}
+
+
+int main(int argc, char **argv)
+{
+    printf("Check it out!\n");
+    TMatrix *c = newMatrix(3, 3);
+    for (unsigned int i = 0; i < c->nrow * c->ncol; i++)
+        c->data[i/c->nrow][i%c->ncol] = i;
+    printMatrix(c);
+    printf("Sorting!\n");
+    mergesort_row(c, 0, &sort);
+    printMatrix(c);
+    return 0;
+}
+
+
+TMatrix* fcfs(TMatrix *c)
+{
+    return NULL;
+}
+
+
+TMatrix* lrjf(TMatrix *c)
+{
+    return NULL;
+}
+
+
+TMatrix* heuristic(TMatrix *c)
+{
+    return NULL;
+}
